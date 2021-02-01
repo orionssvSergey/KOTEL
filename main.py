700242263926eca310237cb3b4ffb25d2fcc1325
sereginSSV <seregin@ssvmail.ru>
2021-02-01 16:10:50 +0600

    Заслонка- работает

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 import socket
+import machine
 import network
 import json
 import time
 from machine import Timer
-from machine import I2C, Pin
+from machine import I2C, Pin, PWM
 from mp_i2c_lcd1602 import LCD1602
 from ds18x20 import DS18X20
 from onewire import OneWire
 
 
+temper_in = 0
+temper_out = 0
+
+
 
 # ИНИЦИАЛИЗАЦИЯ ПИНОВ
-#   датчики температуры
-sensor_DS18X20 = Pin(25, Pin.IN, Pin.PULL_UP)  # первый датчик
+sensor_DS18X20 = Pin(25, Pin.IN, Pin.PULL_UP)  # датчики температуры
+pin_servo = Pin(17, Pin.OUT)  # управление серверов
 
+# инициализация прина управления сервоприводом
+servo = machine.PWM(pin_servo, freq=50)
 
 # инициализация экрана
 i2c = I2C(1, sda=Pin(21), scl=Pin(22))
@@ -27,11 +34,33 @@ roms = temp.scan()
 temp.convert_temp()
 
 
+# Управление сервоприводом 1-откр 0-закр
+def servo_state(state=0):
+    if state == 1:
+        servo.duty(40)
+    if state == 0:
+        servo.duty(90)
+
+
 while True:
-    time.sleep(1)
     temp.convert_temp()
-    lcd.puts(int(temp.read_temp(roms[0])), 0, 0)
-    lcd.puts(int(temp.read_temp(roms[1])), 0, 1)
+    temper_in = int(temp.read_temp(roms[0]))
+    temper_out = int(temp.read_temp(roms[1]))
+    lcd.puts(temper_in, 0, 0)
+    lcd.puts(temper_out, 0, 1)
+    if temper_out > 33:
+        servo_state(0)
+        lcd.puts('close ', 0, 3)
+
+    else:
+        servo_state(1)
+        lcd.puts('open ', 0, 3)
+
+
+
+
+
+
 
 
 
